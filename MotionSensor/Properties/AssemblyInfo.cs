c9b0baae27f7f1ce2dc440918ff2e60ea39f7c28
Aparajita Dutta <apdutta@microsoft.com>
2015-11-02 12:43:52 -0800

    Adding company name

--- MotionSensor/Properties/AssemblyInfo.cs
+++ MotionSensor/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("MotionSensor")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Microsoft")]
 [assembly: AssemblyProduct("MotionSensor")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
